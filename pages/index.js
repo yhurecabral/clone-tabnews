9b18a6673c168511c9e85623add6610df9963dee
yhurecabral <yhurecabral@gmail.com>
2025-12-07 22:55:30 +0000

    altera frase inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return  <h1>Estou tentando. Vamos ver aonde chego.</h1>
+    return  <h1>Estou tentando. Vamos ver aonde chego. Acho que vai dar bom. Estou otimista.</h1>
 }
 
 export default Home;
